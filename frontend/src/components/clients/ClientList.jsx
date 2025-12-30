d6e78ecdd90a68e03751297051f22a937018fddb
Aakash Jaiswal <jaiswalaakash315@gmail.com>
2025-12-30 17:25:37 +0530

    Add Advanced Search & Filtering

--- frontend/src/components/clients/ClientList.jsx
+++ frontend/src/components/clients/ClientList.jsx
@@ -125,7 +125,7 @@ const ClientList = () => {
       </div>
 
       {/* Search */}
-      <div className="card overflow-visible">
+      <div className="card p-5 overflow-visible">
         <div className="relative" ref={searchRef}>
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
             <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
